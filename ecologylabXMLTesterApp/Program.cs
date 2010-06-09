fa71d5ed4715800865b6cd113b56278047e441e6
nskhan <nskhan@ecologylab.net>
2010-06-09 19:11:26 +0000

    non-working polymorphic test case added.

--- ecologylabXMLTesterApp/Program.cs
+++ ecologylabXMLTesterApp/Program.cs
@@ -4,6 +4,7 @@ using System.Text;
 using ecologylabFundamental.ecologylab.xml.library;
 using System.IO;
 using ecologylabFundamental.ecologylab.xml;
+using ecologylabFundamental.ecologylab.xml.library.Schmannel;
 
 namespace ecologylabXMLTesterApp
 {
@@ -12,6 +13,52 @@ namespace ecologylabXMLTesterApp
         static void Main(string[] args)
         {
 
+            PolymorphicTest();
+
+
+        }
+
+        private static void PolymorphicTest()
+        {
+            /**
+            * 
+            * Simple test case poly-morphic collection 
+            *             
+            */
+            Item item = new Item("t2ec");
+            Item schmItem = new SchmItem("cf");
+            Item nested = new BItem("nested");
+
+            Schmannel schmannel = new Schmannel();
+
+            schmannel.polyItem = nested;
+
+            schmannel.PolyAdd(item);
+            schmannel.PolyAdd(schmItem);
+
+            StringBuilder output = new StringBuilder();
+
+            schmannel.translateToXMLStringBuilder(output);
+
+            // create a writer and open the file
+            TextWriter tw = new StreamWriter("polymorphic_output.xml");
+            tw.WriteLine(output);
+            tw.Close();
+
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+            ElementState es = ElementState.translateFromXML("polymorphic_output.xml", SchmannelTranslations.Get());
+
+            output.Clear();
+            es.translateToXMLStringBuilder(output);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+
+        private static void MonomorphicTest()
+        {
             /**
              * 
              * Simple test case mono-morphic collection 
@@ -22,15 +69,15 @@ namespace ecologylabXMLTesterApp
             Item item1 = new Item();
             Item item2 = new Item();
             List<String> categorySet = new List<String>();
-            
+
             rssState.Version = 1.0f;
-            
+
             channel.Title = "testTile";
             channel.Description = "testDescription";
 
             categorySet.Add("category1");
             categorySet.Add("category2");
-            
+
             item1.Title = "testItem1";
             item1.Description = "testItem1Description";
             item1.CategorySet = categorySet;
@@ -64,8 +111,6 @@ namespace ecologylabXMLTesterApp
             es.translateToXMLStringBuilder(output);
             Console.WriteLine(output);
             Console.ReadLine();
-
-
         }
     }
 }
