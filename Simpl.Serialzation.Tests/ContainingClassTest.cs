28f19dd72d477eabc0ee073fbdc98d237477b242
nskhan <nskhan@ecologylab.net>
2011-10-12 23:06:11 +0000

    more test cases added

---
+++ Simpl.Serialzation.Tests/ContainingClassTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simpl.Serialization;
+using Simpl.Serialization.Library.Inheritence;
+using Simpl.Serialzation.Tests.TestHelper;
+
+namespace Simpl.Serialzation.Tests
+{
+    [TestClass]
+    public class ContainingClassTest
+    {
+        [TestMethod]
+        public void ContainingClassXml()
+        {
+            SimplTypesScope translationScope = SimplTypesScope.Get("test", typeof (ContainingClass),
+                                                                   typeof (ChildClass1), typeof (ChildClass2),
+                                                                   typeof (BaseClass));
+
+            ContainingClass ccb = new ContainingClass {TheField = new BaseClass()};
+
+            TestMethods.TestSimplObject(ccb, translationScope, StringFormat.Xml);
+
+            ContainingClass cc1 = new ContainingClass {TheField = new ChildClass1()};
+
+            TestMethods.TestSimplObject(cc1, translationScope, StringFormat.Xml);
+
+            ContainingClass cc2 = new ContainingClass {TheField = new ChildClass2()};
+
+            TestMethods.TestSimplObject(cc2, translationScope, StringFormat.Xml);
+        }
+
+        //        [TestMethod]
+        public void ContainingClassJson()
+        {
+            SimplTypesScope translationScope = SimplTypesScope.Get("test", typeof (ContainingClass),
+                                                                   typeof (ChildClass1), typeof (ChildClass2),
+                                                                   typeof (BaseClass));
+
+            ContainingClass ccb = new ContainingClass {TheField = new BaseClass()};
+
+            TestMethods.TestSimplObject(ccb, translationScope, StringFormat.Json);
+
+            ContainingClass cc1 = new ContainingClass {TheField = new ChildClass1()};
+
+            TestMethods.TestSimplObject(cc1, translationScope, StringFormat.Json);
+
+            ContainingClass cc2 = new ContainingClass {TheField = new ChildClass2()};
+
+            TestMethods.TestSimplObject(cc2, translationScope, StringFormat.Json);
+        }
+    }
+}
---
+++ Simpl.Serialzation.Tests/ItemContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simpl.Serialization;
+using Simpl.Serialization.Library.Items;
+using Simpl.Serialzation.Tests.TestHelper;
+
+namespace Simpl.Serialzation.Tests
+{
+    [TestClass]
+    public class ItemContainer
+    {
+        [TestMethod]
+        public void ItemContainerXml()
+        {
+            Container c = new Container();
+            c.PopulateContainer();
+
+            SimplTypesScope containerTranslationScope = SimplTypesScope.Get("containerScope",
+                                                                            typeof (Container), typeof (ItemBase),
+                                                                            typeof (ItemOne), typeof (ItemTwo),
+                                                                            typeof (ItemRandom));
+
+            TestMethods.TestSimplObject(c, containerTranslationScope, StringFormat.Xml);
+        }
+    }
+}
