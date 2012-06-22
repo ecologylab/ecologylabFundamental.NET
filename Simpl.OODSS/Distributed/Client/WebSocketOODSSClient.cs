b8148b3b0664106823aa5f96a8c5a1a49e8c8a38
shenfeng <shenfeng@ecologylab.net>
2012-06-22 23:33:46 +0000

    client and messageWithMetadata in progress

--- Simpl.OODSS/Distributed/Client/WebSocketOODSSClient.cs
+++ Simpl.OODSS/Distributed/Client/WebSocketOODSSClient.cs
@@ -4,21 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using Simpl.OODSS.Distributed.Common;
 using Simpl.OODSS.Distributed.Impl;
 using Simpl.OODSS.Messages;
 using Simpl.Serialization;
+using WebSocket4Net;
 using ecologylab.collections;
 
 namespace Simpl.OODSS.Distributed.Client
 {
-    public class WebSocketOODSSClient:BaseClient
+    public class WebSocketOODSSClient : BaseClient
     {
         #region Data Member
 
         protected string ServerAddress;
 
-        protected Scope<object> OjectRegistry;
+        protected Scope<object> ObjectRegistry;
 
         protected readonly StringBuilder RequestBuffer;
         protected readonly StringBuilder IncomingMessageBuffer;
@@ -29,13 +31,17 @@ namespace Simpl.OODSS.Distributed.Client
         private readonly StringBuilder _currentKeyHeaderSequence = new StringBuilder();
 
         private MessageWithMetadata<ServiceMessage, object> _response = null;
-        private volatile bool _blockingRequestPending = false;        
-        private readonly BlockingCollection<MessageWithMetadata<ServiceMessage,object>>  
+        private volatile bool _blockingRequestPending = false;
+
+        private readonly BlockingCollection<MessageWithMetadata<ServiceMessage, object>>
             _blockingMessageWithMetadataQueue = new BlockingCollection<MessageWithMetadata<ServiceMessage, object>>();
-        private BlockingCollection<ResponseMessage> 
-            _blockingResponseMessageQueue = new BlockingCollection<ResponseMessage>(); 
+
+        private BlockingCollection<ResponseMessage>
+            _blockingResponseMessageQueue = new BlockingCollection<ResponseMessage>();
+
         protected BlockingCollection<PreppedRequest>
             BlockingPreppedRequestQueue = new BlockingCollection<PreppedRequest>();
+
         protected readonly Dictionary<long, PreppedRequest> UnfulfilledRequests = new Dictionary<long, PreppedRequest>();
 
         protected int ReconnectAttemps = ClientConstants.ReconnectAttempts;
@@ -76,18 +82,159 @@ namespace Simpl.OODSS.Distributed.Client
 
         private SimplTypesScope _translationScope;
 
-
+        private bool _firstMessageSent;
+        private ElementState _lastElementState;
 
         #endregion
 
+        #region WebSocketComponent
+
+        private AutoResetEvent _messageReceiveEvent = new AutoResetEvent(false);
+        private AutoResetEvent _dataReceiveEvent = new AutoResetEvent(false);
+        private AutoResetEvent _openedEvent = new AutoResetEvent(false);
+        private AutoResetEvent _closeEevnt = new AutoResetEvent(false);
+
+        private WebSocket _webSocketClient;
+
+        protected string CurrentMessage { get; private set; }
+        protected byte[] CurrentData { get; private set; }
+
+        #endregion WebSocketComponent
+
+        #region Constructor
+
+        public WebSocketOODSSClient(String uri, SimplTypesScope translationScope,
+                                    WebSocketVersion version = WebSocketVersion.Rfc6455) : base()
+        {
+            _webSocketClient = new WebSocket(uri, "basic", version);
+            _webSocketClient.Opened += webSocketClient_Opened;
+            _webSocketClient.Closed += webSocketClient_Closed;
+            _webSocketClient.DataReceived += webSocketClient_DataReceived;
+            _webSocketClient.MessageReceived += webSocketClient_MessageReceived;
+
+            _webSocketClient.Open();
+            if (!_openedEvent.WaitOne(2000))
+                Console.WriteLine("Handshake failed");
+        }
+
+        #endregion Constructor
+
+        #region OODSS methods
+
+        public bool Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool ConnectImpl()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void Reconnect()
+        {
+        }
+
+
+
+        //No blocking
         public void SendMessage(RequestMessage request)
         {
             throw new NotImplementedException();
         }
 
-        private void ProcessUpdate(UpdateMessage message)
+        private void ProcessUpdate(UpdateMessage updateMessage)
         {
-            message.ProcessUpdate(OjectRegistry);
+            updateMessage.ProcessUpdate(ObjectRegistry);
         }
+
+        private void ProcessResponse(ResponseMessage responseMessage)
+        {
+            responseMessage.ProcessResponse(ObjectRegistry);
+        }
+
+        private MessageWithMetadata<ServiceMessage, object> ProcessString(string incomingMessage, int incomingUid)
+        {
+            _response = TranslateStringToServiceMessage(incomingMessage, incomingUid);
+            if (_response == null)
+            {
+                Console.WriteLine("Deserialized failed");
+            }
+            else
+            {
+                if(_response.Message is ResponseMessage)
+                {
+                    Console.WriteLine("Got a response message");
+                    ProcessResponse((ResponseMessage) _response.Message);
+
+                    // TODO: someting related to unfilfilledRequests blah
+                }
+                else if (_response.Message is UpdateMessage)
+                {
+                    Console.WriteLine("Got an updateMessage");
+                    ProcessUpdate((UpdateMessage) _response.Message);
+                }
+            }
+            return _response;
+        }
+
+        private MessageWithMetadata<ServiceMessage, object> TranslateStringToServiceMessage(string incomingMessage, int incomingUid)
+        {
+            ServiceMessage responseMessage = (ServiceMessage) _translationScope.Deserialize(incomingMessage, StringFormat.Json);
+            if (responseMessage == null)
+                return null;
+
+            MessageWithMetadata<ServiceMessage, object> returnValue = ResponsePool.Acquire();
+
+            returnValue.Message = responseMessage;
+            returnValue.Uid = incomingUid;
+            return returnValue;
+        }
+
+        #endregion OODSS methods
+
+        #region WebSocket Handler
+        void webSocketClient_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            if (!_firstMessageSent)
+            {
+                _firstMessageSent = true;
+                return;
+            }
+            CurrentMessage = e.Message;
+            Console.WriteLine("Got the message: " + CurrentMessage);
+
+            _lastElementState = (ElementState) _translationScope.Deserialize(CurrentMessage, StringFormat.Json);
+
+            var lastUpdateMessage = _lastElementState as UpdateMessage;
+            if (lastUpdateMessage != null)
+            {
+                Console.WriteLine("This is an update message...");
+
+                ProcessUpdate(lastUpdateMessage);
+            }
+
+            _messageReceiveEvent.Set();
+        }
+
+        void webSocketClient_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            CurrentData = e.Data;
+            _dataReceiveEvent.Set();
+        }
+
+        void webSocketClient_Closed(object sender, EventArgs e)
+        {
+            // TODO: handle client close event;
+            _closeEevnt.Set();
+        }
+
+        void webSocketClient_Opened(object sender, EventArgs e)
+        {
+            _openedEvent.Set();
+        }
+
+        #endregion WebSocket Handler
     }
+
 }
--- Simpl.OODSS/Distributed/Impl/MessageWithMetadata.cs
+++ Simpl.OODSS/Distributed/Impl/MessageWithMetadata.cs
@@ -8,9 +8,46 @@ namespace Simpl.OODSS.Distributed.Impl
 {
     class MessageWithMetadata<M, A> : IComparable<MessageWithMetadata<M, A>> where M:ServiceMessage
     {
+        private long _uid;
+
+        private M _message;
+        private A _attachment;
+
+        public long Uid
+        {
+            get { return _uid; }
+            set { _uid = value; } 
+        }
+        public M Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+        public A Attachment
+        {
+            get { return _attachment; }
+            set { _attachment = value; } 
+        }
+
+        public MessageWithMetadata(M response, long uid=-1, A attachment=default(A))
+        {
+            Uid = uid;
+            Attachment = attachment;
+            Message = response;
+        }
+
         public int CompareTo(MessageWithMetadata<M, A> other)
         {
-            throw new NotImplementedException();
+            return (int) (_uid - other._uid);
         }
+
+        public void Clear()
+        {
+            Uid = -1;
+            Message = null;
+            Attachment = default(A);
+        }
+
+
     }
 }
