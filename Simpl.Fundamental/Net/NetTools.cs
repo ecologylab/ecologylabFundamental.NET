1af2a004165844b0faff14c98c0a765467d339ad
shenfeng <shenfeng@ecologylab.net>
2012-06-22 21:13:03 +0000

    added NetTools.cs to privide network related helper functions

---
+++ Simpl.Fundamental/Net/NetTools.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Simpl.Fundamental.Net
+{
+    public class NetTools
+    {
+        public static IPAddress[] WrapSingleAddress(IPAddress address)
+        {
+            IPAddress[] wrappedAddress = {address};
+            return wrappedAddress;
+        }
+
+        public static IPAddress[] GetAllIPAddressesForLocalhost()
+        {
+            return Dns.GetHostAddresses(Dns.GetHostName());
+        }
+    }
+}
