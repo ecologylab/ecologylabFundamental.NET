c3f2b5312e2565a8fffa757cddd8c29637c0f6c4
shenfeng <shenfeng@ecologylab.net>
2012-06-26 07:54:07 +0000

    added Simpl.Fundamental/Collections/ResourcePoolWithSize.cs

---
+++ Simpl.Fundamental/Collections/ResourcePoolWithSize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simpl.Fundamental.Collections
+{
+    public abstract class ResourcePoolWithSize<T>: ResourcePool<T>
+    {
+        protected int ResourceObjectCapacity;
+
+        public ResourcePoolWithSize(int initialCapacity, int resourceObjectCapacity) : base(initialCapacity)
+        {
+            ResourceObjectCapacity = resourceObjectCapacity;
+        }
+    }
+}
