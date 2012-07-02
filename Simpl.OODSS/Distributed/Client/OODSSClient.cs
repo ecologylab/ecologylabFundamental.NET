694150deeb44cb6fbf5a9a7330fc8736354db045
damaraju <damaraju@ecologylab.net>
2012-07-02 21:37:13 +0000

    New C# OODSS client Now completely asynchronous, with no sleeps Two threads, one for receive, one for send. Socket communication itself is again asynchronous using .NET SendAsync and ReceiveAsync calls. Handles Update and Response messages.

--- Simpl.OODSS/Distributed/Client/OODSSClient.cs
+++ Simpl.OODSS/Distributed/Client/OODSSClient.cs
@@ -5,8 +5,10 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Simpl.Fundamental.Generic;
 using Simpl.OODSS.Messages;
 using Simpl.Serialization;
+using ecologylab.collections;
 using ecologylab.serialization;
 
 /* 
@@ -20,146 +22,334 @@ namespace Simpl.OODSS.Distributed.Client
 {
     public class OODSSClient 
     {
-        public Socket clientSocket;
+        private Socket _clientSocket;
 
-        public Task StartClient(string host,int port) 
+        private readonly Thread _sendMesssageThread;
+        private readonly Thread _receiveMesssageThread;
+        
+        private readonly BlockingCollection<QueueObject> _requestQueue;
+
+        private readonly ConcurrentDictionary<int, QueueObject> _pendingRequests;
+
+        static int _uid = 1;
+
+        private CancellationTokenSource _cancellationTokenSource;
+
+        private static readonly SimplTypesScope ServicesTranslationScope = DefaultServicesTranslations.Get();
+
+        private bool _isRunning = true;
+
+        private static ManualResetEventSlim SendDone = new ManualResetEventSlim(false);
+        private static ManualResetEventSlim ReceiveDone = new ManualResetEventSlim(false);
+
+        private static string _response = String.Empty;
+
+        public string Host { get; private set; }
+        public int Port { get; private set; }
+        public SimplTypesScope SimplTypesScope { get; set; }
+        public Scope<object> ObjectRegistry { get; set; }
+
+        public OODSSClient(String host, int port, SimplTypesScope simplTypesScope, Scope<object> objectRegistry)
         {
+            _sendMesssageThread         = new Thread(SendMessageWorker);
+            _receiveMesssageThread      = new Thread(ReceiveMessageWorker);
+            _pendingRequests            = new ConcurrentDictionary<int, QueueObject>();
+            _requestQueue               = new BlockingCollection<QueueObject>(new ConcurrentQueue<QueueObject>());
+
+            Host                        = host;
+            Port                        = port;
+            SimplTypesScope             = simplTypesScope;
+            ObjectRegistry              = objectRegistry;
+        }
+
+        public void Start() 
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            _cancellationTokenSource.Token.Register(PerformDisconnect);
+
             try 
             {
-                IPHostEntry ipHostInfo = Dns.Resolve(host);//"achilles.cse.tamu.edu");
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Host);//"achilles.cse.tamu.edu");
                 IPAddress ipAddress = ipHostInfo.AddressList[0];
-                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
-                clientSocket = new Socket(AddressFamily.InterNetwork,  SocketType.Stream, ProtocolType.Tcp);
-                clientSocket.Connect(host, port);            
-            } catch (Exception e) {
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, Port);
+                _clientSocket = new Socket(AddressFamily.InterNetwork,  SocketType.Stream, ProtocolType.Tcp);
+                _clientSocket.Connect(Host, Port);
+
+                Console.WriteLine("Client socket with host ({0}) connected? {1}.", Host, _clientSocket.Connected);
+
+                var initState = new InitConnectionRequest();
+                int uid = _uid;
+                QueueObject q = new QueueObject(initState, uid, null);
+
+                _requestQueue.Add(q);
+                _sendMesssageThread.Start();
+                _receiveMesssageThread.Start();
+            } 
+            catch (Exception e)
+            {
                 Console.WriteLine(e.ToString());
             }
-            return null;
         }
-    
-        public ElementState GetResponse(ElementState request,SimplTypesScope ts=null)
+
+
+        public void StopClient()
         {
-            SimplTypesScope translationScope = DefaultServicesTranslations.Get();
-            if (ts != null)
-                translationScope.AddTranslations(ts); 
-            ElementState responseElementState = null;
+            _isRunning = false;
+            _cancellationTokenSource.Cancel();
+        }
+
+        public void PerformDisconnect()
+        {
+            Console.WriteLine("Performing Disconnect");
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="requestMessage"> </param>
+        /// <returns></returns>
+        public async Task<ResponseMessage> RequestAsync(RequestMessage requestMessage)
+        {
+            TaskCompletionSource<ResponseMessage> tcs = new TaskCompletionSource<ResponseMessage>();
+            int uid = _uid++;
+
+            QueueObject queueRequest = new QueueObject(requestMessage, uid, tcs);
+
+            _pendingRequests.Put(uid, queueRequest);
+            _requestQueue.Add(queueRequest);
+            
+            return await tcs.Task;
+        }
+
+        private string MessageForXml(ElementState requestObj, int uidUsed)
+        {
+            string msg = SimplTypesScope.Serialize(requestObj, StringFormat.Xml);
+            string ret = String.Format("content-length:{0}\r\nuid:{1}\r\n\r\n{2}", msg.Length, uidUsed, msg);
+            return ret;
+        }
+
+        private class QueueObject
+        {
+            public RequestMessage RequestMessage { get; private set; }
+            public int Uid { get; private set; }
+            public TaskCompletionSource<ResponseMessage> Tcs { get; private set; }
+
+            public QueueObject(RequestMessage requestMessage, int uid, TaskCompletionSource<ResponseMessage> tcs )
+            {
+                RequestMessage = requestMessage;
+                Uid = uid;
+                Tcs = tcs;
+            }
+        }
+
+
+        private class ReceiveStateObject
+        {
+            // Client socket.
+            public Socket workSocket = null;
+            // Size of receive buffer.
+            public const int BufferSize = 1024*1024;
+            // Receive buffer.
+            public byte[] buffer = new byte[BufferSize];
+            // Received data string.
+            public StringBuilder sb = new StringBuilder();
+        }
+
+        private static void Receive(Socket client)
+        {
+            ReceiveDone = new ManualResetEventSlim(false);
             try
             {
-                //push
-                
-                string rq = SimplTypesScope.Serialize(request, StringFormat.Xml);
+                // Create the state object.
+                ReceiveStateObject state = new ReceiveStateObject {workSocket = client};
 
-                String sendMessage = message_for_xml(rq);
-                Console.WriteLine("Trying to send the string:" + sendMessage);
-                clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
-            
-
-                //pull
-                byte[] data = new byte[1024*1024];
-                int receivedDataLength = clientSocket.Receive(data);
-                string stringData = Encoding.ASCII.GetString(data, 0, receivedDataLength);
-            
-
-                stringData = stringData.Substring(stringData.IndexOf('<')-1);
-                Console.WriteLine(stringData);
-                responseElementState = (ElementState) translationScope.Deserialize(stringData, StringFormat.Xml);
-                return responseElementState;
+                // Begin receiving the data from the remote device.
+                client.BeginReceive(state.buffer, 0, ReceiveStateObject.BufferSize, 0,
+                    new AsyncCallback(ReceiveCallback), state);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
-            return responseElementState;
-        }
-    
-        static int n=1;
-        public string message_for_xml(string x)
-        {
-            string ret = "content-length:" + x.Length + "\r\nuid:" + n + "\r\n\r\n" + x;
-            n++;
-            return ret;
         }
 
-        public OODSSClient()
+        private static void ReceiveCallback(IAsyncResult ar)
         {
-        }
-
-        Thread workingThread;
-        static int callNumber = 0;    
-        public OODSSClient(String host, int port)
-        {
-            StartClient(host, port);
-            GetResponse(new InitConnectionRequest());
-            workingThread = new Thread(new ThreadStart(messageWorker));
-            blockingQueue = new BlockingCollection<QueueObject>();
-            blockingResponse = new BlockingCollection<ElementState>();
-            workingThread.Start();
-        
-        }
-
-        int lastServed = -1;
-        public async Task<ElementState> GetRequestAsync(ElementState elementState, SimplTypesScope translationScope)
-        {
-            int callNum = callNumber;
-        
-            //queue.Add(new QueueObject(elementState,translationScope,callNumber));
-            callNumber+=1;
-            await TaskEx.Run(() => pauseTillServedBlah(callNum));
-            QueueObject toTheQueue = new QueueObject(elementState, translationScope, callNumber);
-            blockingQueue.Add(toTheQueue);
-            ElementState elementStateResponse = null;
-            await TaskEx.Run(() => elementStateResponse=blockingResponse.Take());
-            lastServed = callNum;
-            return elementStateResponse;
-        }
-
-        void pauseTillServedBlah(int i)
-        {
-            while (true)
+            try
             {
-                if (lastServed == i - 1)
-                    break;
-                Thread.Sleep(50);
+                // Retrieve the state object and the client socket 
+                // from the asynchronous state object.
+                ReceiveStateObject state = (ReceiveStateObject)ar.AsyncState;
+                Socket client = state.workSocket;
+                // Read data from the remote device.
+                int bytesRead = client.EndReceive(ar);
+                if (bytesRead > 0)
+                {
+                    // There might be more data, so store the data received so far.
+                    string value = Encoding.ASCII.GetString(state.buffer, 0, bytesRead);
+                    
+                    state.sb.Append(value);
+                    //  Get the rest of the data.
+                    Console.WriteLine("Got so far:\n"+value);
+
+                    ProcessMessageString(value);
+//                    else
+//                    {
+//                        Console.WriteLine("Getting More data. ");
+//                        client.BeginReceive(state.buffer, 0, ReceiveStateObject.BufferSize, 0, ReceiveCallback, state);    
+//                    }
+                }
+                else
+                {
+//                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                    // All the data has arrived; put it in response.
+                    if (state.sb.Length > 1)
+                    {
+                        _response = state.sb.ToString();
+                    }
+                    // Signal that all bytes have been received.
+                    ReceiveDone.Set();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
             }
         }
 
-        struct QueueObject
+        private static void ProcessMessageString(string value)
         {
-            public ElementState elementState;
-            public SimplTypesScope translationScope;
-            public int callNumber;
-            public ElementState response;
-            public QueueObject(ElementState elementState, SimplTypesScope translationScope, int callNumber)
+            int msgLengthIndex = value.IndexOf(":") + 1;
+            string intString = value.Substring(msgLengthIndex, value.IndexOf("\r\n", msgLengthIndex) - msgLengthIndex);
+            int msgLength = int.Parse(intString);
+            if (msgLength > 0)
             {
-                this.elementState = elementState;
-                this.translationScope = translationScope;
-                this.callNumber = callNumber;
-                response = null;
+                int expectedLength = value.IndexOf("<") + msgLength;
+                _response = value.Substring(0, expectedLength);
+                ReceiveDone.Set();
+                if (value.Length > expectedLength)
+                {
+                    string sb = value.Substring(expectedLength);
+                    ProcessMessageString(sb);
+                }
             }
         }
 
-        BlockingCollection<QueueObject> blockingQueue;
-        BlockingCollection<ElementState> blockingResponse;
-        public void messageWorker()
-        {
-            while (true)
-            {
-                QueueObject q = blockingQueue.Take();
-                blockingResponse.Add( GetResponse(q.elementState,q.translationScope));
+        private static void Send(Socket client, String data) {
+            // Convert the string data to byte data using ASCII encoding.
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.
+            client.BeginSend(byteData, 0, byteData.Length, 0, SendCallback, client);
+        }
+
+        private static void SendCallback(IAsyncResult ar) {
+            try {
+                // Retrieve the socket from the state object.
+                Socket client = (Socket) ar.AsyncState;
+
+                // Complete sending the data to the remote device.
+                int bytesSent = client.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+
+                // Signal that all bytes have been sent.
+                SendDone.Set();
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
             }
         }
 
-        public void doWork()
+        private void SendMessageWorker()
         {
-            //while (true)
-            for (int i = 0; i < 5; i++ )
+            Console.WriteLine("Entering OODSS Send Message loop");
+            while (_isRunning)
             {
-                Console.WriteLine("derp");
-                Thread.Sleep(500);
-                Console.WriteLine("dop");
-            }
-            // return 0;
+                try
+                {
+                    //Console.WriteLine("Top of loop" );
+                    //Hold the thread here until it recieves a request to be processed.
+                    QueueObject q = _requestQueue.Take(_cancellationTokenSource.Token);
 
+                    //Push pull
+                    Console.WriteLine("Trying to send the string:" + q.Uid);
+
+                    SendDone = new ManualResetEventSlim(false);
+                    string messageForXml = MessageForXml(q.RequestMessage, q.Uid);
+                    Send(_clientSocket, messageForXml);
+                    SendDone.Wait(_cancellationTokenSource.Token);
+                    Console.WriteLine("--- Sent Request : {0}", q.Uid);
+                }
+                catch (OperationCanceledException e)
+                {
+                    Console.WriteLine("The operation was cancelled." + e.TargetSite);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Caught Exception :\n " + e.StackTrace);
+                }
+            }
+
+            Console.WriteLine("Performing disconnect");
+
+        }
+
+        private void ReceiveMessageWorker()
+        {
+            Console.WriteLine("Entering OODSS Recieve Message loop");
+            while (_isRunning)
+            {
+                try
+                {
+                    Receive(_clientSocket);
+                    ReceiveDone.Wait(_cancellationTokenSource.Token);
+
+                    int uidIndex = _response.IndexOf("uid:") + 4;
+                    string intString = _response.Substring(uidIndex, _response.IndexOf("\r\n", uidIndex) - uidIndex);
+                    int responseUid = int.Parse(intString);
+
+                    ServiceMessage serviceMessage = (ServiceMessage)SimplTypesScope.Deserialize(_response.Substring(_response.IndexOf('<')), StringFormat.Xml);
+                    if (serviceMessage == null)
+                    {
+                        throw new ArgumentNullException(string.Format("Received a null {0} object. Deserialization failed?", "serviceMessage"));
+                    }
+
+                    if (serviceMessage is UpdateMessage)
+                    {
+                        var updateMessage = serviceMessage as UpdateMessage;
+                        updateMessage.ProcessUpdate(ObjectRegistry);
+                    }
+                    else if (serviceMessage is ResponseMessage)
+                    {
+                        var responseMessage = serviceMessage as ResponseMessage;
+                        responseMessage.ProcessResponse(ObjectRegistry);
+
+                        QueueObject queueObject;
+                        _pendingRequests.TryGetValue(responseUid, out queueObject);
+                        if (queueObject == null)
+                        {
+                            Console.WriteLine("No pending request with Uid: {0}", responseUid);
+                        }
+                        else
+                        {
+                            TaskCompletionSource<ResponseMessage> taskCompletionSource = queueObject.Tcs;
+                            if (taskCompletionSource != null)
+                            {
+                                Console.WriteLine("--- Finished Request : {0}", queueObject.Uid);
+                                taskCompletionSource.TrySetResult(responseMessage);
+                            }
+                        }
+                    }
+                }
+                catch (OperationCanceledException e)
+                {
+                    Console.WriteLine("The operation was cancelled." + e.TargetSite);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Caught Exception :\n " + e.StackTrace);
+                }
+            }
         }
     }
 }
